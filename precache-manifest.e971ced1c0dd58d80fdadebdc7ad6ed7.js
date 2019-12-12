6339e271dfd87418f6c1cf0d6656fdb2a6691ec5
thanks4all <giovanni.montenegro@aurigaspa.com>
2019-12-12 15:36:30 +0100

    Updates

---
+++ precache-manifest.e971ced1c0dd58d80fdadebdc7ad6ed7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "214a4465d9722bed682fc3278ec3e5e6",
+    "url": "/thanks4all/thanks4all.github.io/index.html"
+  },
+  {
+    "revision": "754f84ae7fada057be85",
+    "url": "/thanks4all/thanks4all.github.io/static/css/main.f7bd1c68.chunk.css"
+  },
+  {
+    "revision": "04d0bb2b60c43e0d9866",
+    "url": "/thanks4all/thanks4all.github.io/static/js/2.3f2340d0.chunk.js"
+  },
+  {
+    "revision": "c30c8740d3e1cfa20f5f83b5f0c51ffc",
+    "url": "/thanks4all/thanks4all.github.io/static/js/2.3f2340d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "754f84ae7fada057be85",
+    "url": "/thanks4all/thanks4all.github.io/static/js/main.f43a827d.chunk.js"
+  },
+  {
+    "revision": "3de0c36745737d43f78b",
+    "url": "/thanks4all/thanks4all.github.io/static/js/runtime-main.be36456b.js"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/thanks4all/thanks4all.github.io/precache-manifest.e971ced1c0dd58d80fdadebdc7ad6ed7.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/thanks4all/thanks4all.github.io/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
+});
